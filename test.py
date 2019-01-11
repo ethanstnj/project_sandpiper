932cf476f546234ab136a1a74fd8bc0719b742b1
Ethan Tuckman <ethanstnj@gmail.com>
2019-01-11 13:21:34 -0500

    fixed authentication

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-# Ian Annase
-# 4/16/18
+# Ethan Tuckman
+# project_sandpiper
+# January 10, 2019
+
+#Ethan's Spotify user ID: 121928206
 
 import os
 import sys
+import pprint
 import json
 import spotipy
 import webbrowser
@@ -10,17 +14,41 @@ import spotipy.util as util
 from json.decoder import JSONDecodeError
 
 # Get the username from terminal
-username = sys.argv[1]
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username/userID" %(sys.argv[0],))
+    sys.exit()
 
-#get user id 121928206
+#set token and scope
+#Scopes provide Spotify users using third-party apps the confidence that only the information they choose to share will be shared
+scope = ''
+token = util.prompt_for_user_token(username,scope)
 
-# Erase cache adn prompt for user permission
+if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.current_user_playlists(limit=50)
+    for i, item in enumerate(results['items']):
+        print("%d %s" %(i, item['name']))
+else:
+    print("Can't get token for", username)
+
+
+
+#########################################################
+
+#had to manually upgrade version of spotipy by downloading directly from github
+#before that we used this code as a workaround:
+
+""" # Erase cache and prompt for user permission
 try:
-    token = util.prompt_for_user_token(username)
+    token = util.prompt_for_user_token(username,scope)
 except:
     os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username)
+    token = util.prompt_for_user_token(username,scope)
+
+print("we made it!")
 
 #Create our spotifyObject
-
-spotifyObject = spotipy.Spotify(auth=token)
+spotifyObject = spotipy.Spotify(auth=token) """
